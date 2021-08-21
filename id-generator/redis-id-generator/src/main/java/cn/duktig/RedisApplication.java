4b34a7c9f4844c0979201a98aac0b4b08461ef86
duktig666(RenShiWei) <1487660836@qq.com>
2021-08-21 14:13:09 +0800

    redis

---
+++ id-generator/redis-id-generator/src/main/java/cn/duktig/RedisApplication.java
@@ -0,0 +1,19 @@
+package cn.duktig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description:Redis模块启动程序
+ *
+ * @author RenShiWei    `
+ * Date: 2021/7/25 14:22
+ **/
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+
+}
+
