26c37ab15044fe611f1f736cea9f32a4864b26ce
duktig666(RenShiWei) <1487660836@qq.com>
2021-09-13 17:47:30 +0800

    测试几种负载均衡算法的执行时间，进行对比

--- load-balance/load-balance-algorithm/src/test/java/cn/duktig/loadbalance/LoadBalanceTest.java
+++ load-balance/load-balance-algorithm/src/test/java/cn/duktig/loadbalance/LoadBalanceTest.java
@@ -4,6 +4,8 @@ import cn.duktig.loadbalance.algorithm.*;
 import cn.duktig.loadbalance.utils.LoadBalanceUtil;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 public class LoadBalanceTest {
@@ -58,5 +60,41 @@ public class LoadBalanceTest {
         LoadBalanceUtil.countRoutingMap(routingMap);
     }
 
+    /**
+     * 对比 几种负载均衡算法的时间
+     */
+    @Test
+    public void testLoadBalanceTime() {
+        System.out.println("---测试10W访问量，各种负载均衡算法用时---");
+        LocalDateTime startTime = LocalDateTime.now();
+        LoadBalanceUtil.imitateRouting(new RoundRobinLoadBalance(), 100000);
+        LocalDateTime endTime = LocalDateTime.now();
+        long minutes = Duration.between(startTime, endTime).toMillis();
+        System.out.println("轮询负载均衡算法 用时：" + minutes + " ms");
+
+        startTime = LocalDateTime.now();
+        LoadBalanceUtil.imitateRouting(new RandomLoadBalance(), 100000);
+        endTime = LocalDateTime.now();
+        minutes = Duration.between(startTime, endTime).toMillis();
+        System.out.println("随机负载均衡算法 用时：" + minutes + " ms");
+
+        startTime = LocalDateTime.now();
+        LoadBalanceUtil.imitateRouting(new WeightRoundRobinLoadBalance(), 100000);
+        endTime = LocalDateTime.now();
+        minutes = Duration.between(startTime, endTime).toMillis();
+        System.out.println("加权轮询负载均衡算法 用时：" + minutes + " ms");
+
+        startTime = LocalDateTime.now();
+        LoadBalanceUtil.imitateRouting(new WeightRandomLoadBalance(), 100000);
+        endTime = LocalDateTime.now();
+        minutes = Duration.between(startTime, endTime).toMillis();
+        System.out.println("加权随机负载均衡算法 用时：" + minutes + " ms");
+
+        startTime = LocalDateTime.now();
+        LoadBalanceUtil.imitateRouting(new HashLoadBalance(), 100000);
+        endTime = LocalDateTime.now();
+        minutes = Duration.between(startTime, endTime).toMillis();
+        System.out.println("源地址Hash负载均衡算法 用时：" + minutes + " ms");
+    }
 
 }
